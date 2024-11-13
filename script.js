d905e3276bde49368cfcdc39e2cfea0a17674853
Khawaja Zeeshan <khawajazeeshan03488944412@gmail.com>
2024-11-13 12:00:48 +0500

    uddate javascript file

--- script.js
+++ script.js
@@ -2,33 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
     // Check the width of the screen and apply the conditions
     if (window.innerWidth < 500) {
 
-        // Display the line at the bottom of the "For You" Button
+        // Add Event Listener on For You Button...................................
         document.querySelector(".foryoy-btn").addEventListener("click", () => {
-            document.querySelector(".click-line-one").style.display = "block";
-            // Check if click-line-two is visible; if so, hide it
-            if (document.querySelector(".click-line-two").style.display === "block") {
-                document.querySelector(".click-line-two").style.display = "none";
-            }
+            document.querySelectorAll(".click-line-one, .second").forEach(element => {
+                element.style.display = "block";
+            });
+            document.querySelectorAll(".click-line-two, .third").forEach(element => {
+                element.style.display = "none";
+            });
         });
 
-        // Display the line at the bottom of the "Following" Button
+        // Add Event Listener on Following Button...................................
         document.querySelector(".Following-btn").addEventListener("click", () => {
-            document.querySelector(".click-line-one").style.display = "none";
-            document.querySelector(".click-line-two").style.display = "block";
+            document.querySelectorAll(".click-line-one, .second").forEach(element => {
+                element.style.display = "none";
+            });
+            document.querySelectorAll(".click-line-two, .third").forEach(element => {
+                element.style.display = "block";
+            });
         });
 
-        // Display the Following Page
-        document.querySelector(".Following-btn").addEventListener("click", () => {
-            document.querySelector(".second").style.display = "none";
-            document.querySelector(".third").style.display = "block";
-        });
-
-        // Display the For-You Page
-        document.querySelector(".foryoy-btn").addEventListener("click", () => {
-            document.querySelector(".second").style.display = "block";
-            if (document.querySelector(".third").style.display === "block") {
-                document.querySelector(".third").style.display = "none";
-            }
-        });
     }
 });
